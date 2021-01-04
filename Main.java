64e25f9373f98561399517f6b8bf974bc0c7229a
Mahati04 <53967748+Mahati04@users.noreply.github.com>
2021-01-04 16:19:18 +0530

    Add files via upload
    Java program to convert pounds to kgs.

---
+++ Main.java
@@ -0,0 +1,10 @@
+package mahatimadhira;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int pounds=50;
+	    double kgs=pounds*0.45359237;
+        System.out.println("The weight in kgs = " + kgs);
+    }
+}
