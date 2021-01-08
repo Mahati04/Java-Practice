fb095cfe7da509f176efc25bdee3062051eda571
Mahati04 <53967748+Mahati04@users.noreply.github.com>
2021-01-08 15:45:10 +0530

    Add files via upload
    Program to check if a number is Armstrong or not

--- Main.java
+++ Main.java
@@ -1,10 +1,25 @@
 package mahatimadhira;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    int pounds=50;
-	    double kgs=pounds*0.45359237;
-        System.out.println("The weight in kgs = " + kgs);
+    public static void main(String[] args)
+    {
+        int num,temp,rem,result=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number to check for Armstrong property");
+        num=sc.nextInt();
+        temp=num;
+        while(num>0)
+        {
+            rem=num%10;
+            num=num/10;
+            result=result+(rem*rem*rem);
+        }
+        if(temp==result)
+            System.out.println(temp + " is an Armstrong Number");
+        else
+            System.out.println(temp + " is not an Armstrong Number");
     }
 }
